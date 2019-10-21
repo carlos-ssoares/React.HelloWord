06295bafd7d556f03f22c5e4fce1d301d802dddf
carlos-ssoares <carlos.soares@live.com>
2019-10-20 22:09:04 -0400

    adding a change from the feature branch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
  function App() {
-   //var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
-   //return element;
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+    return element;
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 
