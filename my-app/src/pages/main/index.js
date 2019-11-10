c70ce4d2a773f638c65c4dd569a8a954f87989be
carlos-ssoares <carlos.soares@live.com>
2019-11-09 20:54:01 -0400

    acessando api com axios

---
+++ my-app/src/pages/main/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import api from "../../services/api"
+
+export default class Main extends Component {
+    state = {
+        products: [],
+    }
+
+    // e executado assim que o componente e exibido    
+    componentDidMount(){
+        this.loadProducts();
+    };
+
+    loadProducts = async () => {
+        const response = await api.get('/products');
+        
+        this.setState({ products: response.data.docs });
+        // console.log(response.data.docs);
+
+    };
+
+    render() {
+        return(
+            <div className="product-list">
+                {this.state.products.map(product => (
+    <h2 key={product._id}>{product.title}</h2>
+                ))}
+            </div>
+        );
+    }
+}
