2374abef55af127732f60a057460e2ee5b66fb13
carlos-ssoares <carlos.soares@live.com>
2019-11-10 17:37:01 -0400

    Paginacao de consulta utilizando state

--- my-app/src/pages/main/index.js
+++ my-app/src/pages/main/index.js
@@ -5,6 +5,8 @@ import "./styles.css"
 export default class Main extends Component {
     state = {
         products: [],
+        productInfo: {},
+        page: 1,
     }
 
     // e executado assim que o componente e exibido    
@@ -12,16 +14,38 @@ export default class Main extends Component {
         this.loadProducts();
     };
 
-    loadProducts = async () => {
-        const response = await api.get('/products');
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
         
-        this.setState({ products: response.data.docs });
-        // console.log(response.data.docs);
+        // pega docs de response.data, e todo o resto garda em productInfo.
+        const { docs, ...productInfo } = response.data;
 
+        this.setState({ products: docs, productInfo, page });
+        // console.log(response.data.docs);
     };
 
+    prevPage = () => {
+        const { page, productInfo } = this.state;
+
+        if (page === 1) return;
+
+        const pageNumber = page - 1;
+
+        this.loadProducts(pageNumber);
+    }
+
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+
+        if (page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
+    }
+
     render() {
-        const {products } = this.state;
+        const {products, page, productInfo } = this.state;
 
         return(
             <div className="product-list">
@@ -32,6 +56,10 @@ export default class Main extends Component {
                         <a href="">Acessar</a>
                     </article>
                 ))}
+                <div className="actions">
+                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={page === productInfo.pages} onClick={this.nextPage}>Proximo</button>
+                </div>
             </div>
         );
     }
