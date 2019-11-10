ce0ff182678f1f4a0fe121783aeb949438207170
carlos-ssoares <carlos.soares@live.com>
2019-11-10 18:22:03 -0400

    Carregando pagina de detalhe.

--- my-app/src/pages/product/index.js
+++ my-app/src/pages/product/index.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react'
+import api from '../../services/api'
+
+import './styles.css';
 
 export default class Product extends Component {
+    state = {
+        product: {},
+    };
+
+    async componentDidMount() {
+        const { id } = this.props.match.props;
+
+        const response = await api.get(`/products/${id}`);
+
+        this.setState({ product: response.data });
+    }
+
     render() {
-        return <h1>Product</h1>;
+        const { product } = this.state;
+
+        return (
+            <div className="product-info">
+                <h2>{product.title}</h2>
+                <p>{product.description}</p>
+                <p>
+                    URL: <a href={product.url}>{product.url}</a>
+                </p>
+                {/* <h2>Teste</h2>
+                <p>Teste descricao bla bla bla bla bla  bla</p>
+                <p>
+                    URL: <a href="www.google.com">alsdflkasjdflkajsdf</a>
+                </p> */}
+            </div>
+        );
     }
 }
